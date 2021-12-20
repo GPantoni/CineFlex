5d82b1f77b7413cef87bfcac7302c13eeee1cdfa
Glauco Pantoni <glaucopantoni@gmail.com>
2021-12-20 20:16:10 -0300

    <Sucesso /> implementado

--- src/components/Sucesso/index.jsx
+++ src/components/Sucesso/index.jsx
@@ -9,6 +9,23 @@ export default function Sucesso() {
 
     return (
         <Content>
+            <Section>
+                Pedido feito com sucesso!
+            </Section>
+            <Info>
+                <h2>Filme e sessão</h2>
+                <p>{state.filme}</p>
+                <p>{state.data} {state.hora}</p>
+            </Info>
+            <Info>
+                <h2>Ingressos</h2>
+                {state.poltronas.map(pol => <p key={pol}>Assento {pol}</p>)}
+            </Info>
+            <Info>
+                <h2>Comprador</h2>
+                <p>Nome: {state.nome}</p>
+                <p>CPF: {state.cpf}</p>
+            </Info>
             <button onClick={() => navigate('/')}>Voltar pra Home</button>
         </Content>
     )
@@ -24,6 +41,8 @@ const Content = styled.div`
     button {
         width: 225px;
         height: 42px;
+        margin-top: 65px;
+        
         background: #E8833A;
         border: none;
         border-radius: 3px;
@@ -35,5 +54,43 @@ const Content = styled.div`
         text-align: center;
         letter-spacing: 0.04em;
         color: #FFFFFF;
+    }
+`
+const Section = styled.div`
+    width: 60%;
+    margin-top: 50px;
+    text-align: center;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    letter-spacing: 0.04em;
+    color: #247A6B;
+`
+
+const Info = styled.div`
+    width: 100%;
+
+    h2 {
+        margin-top: 40px;
+        margin-bottom: 10px;
+        
+        font-style: normal;
+        font-weight: bold;
+        font-size: 24px;
+        line-height: 28px;
+        letter-spacing: 0.04em;
+        color: #293845;
+    }
+
+    p {
+        margin-bottom: 5px;
+        
+        font-style: normal;
+        font-weight: normal;
+        font-size: 22px;
+        line-height: 26px;
+        letter-spacing: 0.04em;
+        color: #293845;
     }
 `
